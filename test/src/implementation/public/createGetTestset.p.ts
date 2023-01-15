273b666514061ecfdf2f6e4193c7b7889cbb6f3c
corno <corno.astn.a@schraverus.com>
2023-01-15 11:12:43 +0100

    .

--- test/src/implementation/public/createGetTestset.p.ts
+++ test/src/implementation/public/createGetTestset.p.ts
@@ -10,7 +10,25 @@ import * as api from "../../interface"
 import * as pub from "../../../../pub"
 
 export const createGetTestset: api.FCreateGetTestset = ($d) => {
-    return () => {
+    return ($) => {
+
+        pub.$a.readDirectory({ path: ["..", "..", "pub", "src"] })._execute(($) => {
+            switch ($[0]) {
+                case "error":
+                    pl.cc($[1], ($) => {
+
+                    })
+                    break
+                case "success":
+                    pl.cc($[1], ($) => {
+                        $.forEach(() => false, ($, key) => {
+                            pl.logDebugMessage($.path)
+                        })
+                    })
+                    break
+                default: pl.au($[0])
+            }
+        })
 
         const builder = pm.createUnsafeDictionaryBuilder<test.TTestElement>()
         function createTest(name: string, actual: string, expected: string) {
