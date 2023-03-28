8e546e05463b2fd15761b8f9ca444b5a68be4577
corno <corno.astn.a@schraverus.com>
2023-03-28 10:18:23 +0200

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -15,142 +15,145 @@ export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
     'imports': d({
         "common": imp({}),
     }),
-    'types': d({
-        "AnnotatedError": parametrizedType({ "Error": null }, group({
-            "path": member(ref(externalTypeReference("common", "String"))),
-            "error": member(ref(typeParameter("Error"))),
-        })),
-        "Result": parametrizedType({
-            "Error": null,
-            "Success": null,
-        }, taggedUnion({
-            "error": ref(typeParameter("Error")),
-            "success": ref(typeParameter("Success")),
-        })),
-        "AnnotatedReadDirError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("ReadDirError"),
-        }))),
-        "AnnotatedReadFileError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("ReadFileError"),
-        }))),
-        "AnnotatedRmdirError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("RmdirError"),
-        }))),
-        "AnnotatedMkdirError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("MkdirError"),
-        }))),
-        "AnnotatedUnlinkError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("UnlinkError"),
-        }))),
-        "AnnotatedWriteFileError": type(ref(typeReference("AnnotatedError", {
-            "Error": typeReference("WriteFileError"),
-        }))),
-        "WriteFileParameters": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-            "createContainingDirectories": member(boolean()),
-        })),
-        // "AnnotatedReadFileError": group({
-        //     "path": member(str()),
-        //     "error": member(taggedUnion({
-        //         "no entity": null_(),
-        //         "is directory": null_(),
-        //         "unknown": type(group({
-        //             "message": member(str()),
-        //         })),
-        //     })),
-        // }),
-        "DirNodeData": type(group({
-            "path": member(string()),
-            "type": member(taggedUnion({
-                "directory": null_(),
-                "file": null_(),
-                "unknown": null_(),
+    'root': {
+        'namespaces': d({}),
+        'types': d({
+            "AnnotatedError": parametrizedType({ "Error": null }, group({
+                "path": member(ref(externalTypeReference("common", "String"))),
+                "error": member(ref(typeParameter("Error"))),
             })),
-        })),
-        "MkdirError": type(taggedUnion({
-            "no entity": null_(),
-            "exists": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-        "Mkdir_Data": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-            "createContainingDirectories": member(boolean()),
-        })),
-        "Mkdir_Result": type(ref(typeReference("Result", {
-            "Error": typeReference("AnnotatedMkdirError"),
-            "Success": externalTypeReference("common", "Null"),
-        }))),
-        "ReadDirectory_Data": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-        })),
-        "ReadDirectory_Success": type(dictionary(ref(typeReference("DirNodeData")))),
-        "ReadDirectory_Result": type(ref(typeReference("Result", {
-            "Error": typeReference("AnnotatedReadDirError"),
-            "Success": typeReference("ReadDirectory_Success"),
-        }))),
-        "ReadDirError": type(taggedUnion({
-            "no entity": null_(),
-            "is not directory": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-        "ReadFile_Result": type(ref(typeReference("Result", {
-            "Error": typeReference("AnnotatedReadFileError"),
-            "Success": externalTypeReference("common", "String"),
-        }))),
+            "Result": parametrizedType({
+                "Error": null,
+                "Success": null,
+            }, taggedUnion({
+                "error": ref(typeParameter("Error")),
+                "success": ref(typeParameter("Success")),
+            })),
+            "AnnotatedReadDirError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("ReadDirError"),
+            }))),
+            "AnnotatedReadFileError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("ReadFileError"),
+            }))),
+            "AnnotatedRmdirError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("RmdirError"),
+            }))),
+            "AnnotatedMkdirError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("MkdirError"),
+            }))),
+            "AnnotatedUnlinkError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("UnlinkError"),
+            }))),
+            "AnnotatedWriteFileError": type(ref(typeReference("AnnotatedError", {
+                "Error": typeReference("WriteFileError"),
+            }))),
+            "WriteFileParameters": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+                "createContainingDirectories": member(boolean()),
+            })),
+            // "AnnotatedReadFileError": group({
+            //     "path": member(str()),
+            //     "error": member(taggedUnion({
+            //         "no entity": null_(),
+            //         "is directory": null_(),
+            //         "unknown": type(group({
+            //             "message": member(str()),
+            //         })),
+            //     })),
+            // }),
+            "DirNodeData": type(group({
+                "path": member(string()),
+                "type": member(taggedUnion({
+                    "directory": null_(),
+                    "file": null_(),
+                    "unknown": null_(),
+                })),
+            })),
+            "MkdirError": type(taggedUnion({
+                "no entity": null_(),
+                "exists": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+            "Mkdir_Data": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+                "createContainingDirectories": member(boolean()),
+            })),
+            "Mkdir_Result": type(ref(typeReference("Result", {
+                "Error": typeReference("AnnotatedMkdirError"),
+                "Success": externalTypeReference("common", "Null"),
+            }))),
+            "ReadDirectory_Data": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+            })),
+            "ReadDirectory_Success": type(dictionary(ref(typeReference("DirNodeData")))),
+            "ReadDirectory_Result": type(ref(typeReference("Result", {
+                "Error": typeReference("AnnotatedReadDirError"),
+                "Success": typeReference("ReadDirectory_Success"),
+            }))),
+            "ReadDirError": type(taggedUnion({
+                "no entity": null_(),
+                "is not directory": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+            "ReadFile_Result": type(ref(typeReference("Result", {
+                "Error": typeReference("AnnotatedReadFileError"),
+                "Success": externalTypeReference("common", "String"),
+            }))),
 
-        "ReadFile_Data": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-        })),
-        "ReadFileError": type(taggedUnion({
-            "no entity": null_(),
-            "is directory": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-        "RmdirError": type(taggedUnion({
-            "no entity": null_(),
-            "not empty": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-        "UnlinkError": type(taggedUnion({
-            "no entity": null_(),
-            "is directory": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-        "Unlink_Data": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-        })),
-        "Unlink_Result": type(ref(typeReference("Result", {
-            "Error": typeReference("AnnotatedUnlinkError"),
-            "Success": externalTypeReference("common", "Null"),
-        }))),
+            "ReadFile_Data": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+            })),
+            "ReadFileError": type(taggedUnion({
+                "no entity": null_(),
+                "is directory": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+            "RmdirError": type(taggedUnion({
+                "no entity": null_(),
+                "not empty": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+            "UnlinkError": type(taggedUnion({
+                "no entity": null_(),
+                "is directory": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+            "Unlink_Data": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+            })),
+            "Unlink_Result": type(ref(typeReference("Result", {
+                "Error": typeReference("AnnotatedUnlinkError"),
+                "Success": externalTypeReference("common", "Null"),
+            }))),
 
-        // "WriteFile_Result": type(ref(typeReference("Result", {
-        //     "Error": typeReference("AnnotatedWriteFileError"),
-        //     "Success": typeReference("common", "Null"),
-        // })),
+            // "WriteFile_Result": type(ref(typeReference("Result", {
+            //     "Error": typeReference("AnnotatedWriteFileError"),
+            //     "Success": typeReference("common", "Null"),
+            // })),
 
-        "WriteFileData": type(group({
-            "path": member(ref(externalTypeReference("common", "Path"))),
-            "createContainingDirectories": member(boolean()),
-        })),
-        "WriteFileError": type(taggedUnion({
-            "no entity": null_(),
-            "is directory": null_(),
-            "unknown": group({
-                "message": member(string()),
-            }),
-        })),
-    }),
+            "WriteFileData": type(group({
+                "path": member(ref(externalTypeReference("common", "Path"))),
+                "createContainingDirectories": member(boolean()),
+            })),
+            "WriteFileError": type(taggedUnion({
+                "no entity": null_(),
+                "is directory": null_(),
+                "unknown": group({
+                    "message": member(string()),
+                }),
+            })),
+        }),
+    },
     'asynchronous': {
         'interfaces': d({
             //these should be defined somewhere but is this the place
@@ -160,7 +163,7 @@ export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
             // "HandleUnlinkError": interfaceMethod(typeReference("AnnotatedUnlinkError"), null, null, null),
 
 
-            "OnFileWriteError": aInterface( aInterfaceMethod(typeReference("AnnotatedWriteFileError"))),
+            "OnFileWriteError": aInterface(aInterfaceMethod(typeReference("AnnotatedWriteFileError"))),
 
 
             // "Reader": ['group', {
@@ -169,14 +172,14 @@ export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
             //         "onError": builderMethod(typeReference("AnnotatedReadFileError")),
             //     }),
             // }]
-            "StringStreamConsumer": aInterface( streamconsumer(
+            "StringStreamConsumer": aInterface(streamconsumer(
                 aInterfaceMethod(externalTypeReference("common", "String")),
                 aInterfaceMethod(null),
             )),
-            "WriteFile":aInterface( aInterfaceMethod(typeReference("WriteFileParameters"), ['reference', aInterfaceReference("StringStreamConsumer")])),
+            "WriteFile": aInterface(aInterfaceMethod(typeReference("WriteFileParameters"), ['reference', aInterfaceReference("StringStreamConsumer")])),
         }),
         'algorithms': d({
-            "CreateFileWriter": constructor(aInterfaceReference("WriteFile"),  {
+            "CreateFileWriter": constructor(aInterfaceReference("WriteFile"), {
                 "onWriteFileError": aInterfaceReference("OnFileWriteError"),
             }),
             "MakeDirectory": afunction(typeReference("Mkdir_Result"), typeReference("Mkdir_Data")),
@@ -188,6 +191,6 @@ export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
     'synchronous': {
         'interfaces': d({}),
         'algorithms': d({}),
-        
+
     },
 }
